13f3c0de134b5e3ef6f30de2e5fd10bf524682fc
AntonioDeFelipeVargas <antoniodefelipee@gmail.com>
2024-10-11 20:39:06 -0600

    palindrome Complete

---
+++ arraysAndLoops/palindrome/app.js
@@ -0,0 +1,15 @@
+
+const isPalindrome= function (cadena)
+{
+    const cleanString= cadena.toLowerCase().replace(/[^a-z0-9]/g,"");
+    const invertedCleanString=cleanString.split("").reverse().join("");
+    if(cleanString==invertedCleanString)
+        {
+            return ("ES PALINDROMO!")
+        }
+        else { return ("Nones")}
+}
+
+let prueba="Anita lava  tina" ; 
+
+console.log(isPalindrome(prueba));
